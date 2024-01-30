af8b88f0c1c51b91d3f6ada5f4944241fd9648fc
Sheldon Johnson <blacknalljermaine7@gmail.com>
2024-01-30 09:11:24 -0600

    Remove old cold

--- ok.js
+++ ok.js
@@ -1,47 +1,127 @@
+// let rock = "Rock";
+// let paper= "Paper";
+// let scissors="Scissors";
+// let computerSelection;
+// let playerSelection;
+
+//    function numGen() {
+//       let randoNum = Math.floor(Math.random() * 100);
+//       return randoNum;}
+
+//  let getComputerChoice = function(rock, paper, scissors, numGen) {
+//    let result = numGen();
+//    if (result >20 && result<45) {
+//      console.log(paper);
+//    } else if (result > 75 && result<=80) {
+//      console.log(scissors);
+//    } else {
+//      console.log(rock);
+//    }
+// }
+
+// function roundOne(playerSelection,computerSelection){
+
+// playerSelection =  prompt('Rock,Paper,Or Scissors?');
+
+// console.log(playerSelection);
+
+// computerSelection= getComputerChoice(rock,paper,scissors,numGen);
+
+// console.log(computerSelection)
+
+// if(playerSelection==rock && computerSelection==paper){
+
+//   console.log( 'you lose, paper beats rock')
+// }
+
+// else if(playerSelection==paper &&computerSelection==rock  ){
+// console.log('You win, paper beats rock')
+
+// }
+
+// else if(playerSelection==rock && computerSelection==scissors ){
+//   console.log('You win, rock beats scissors')
+// }
+
+// else if(playerSelection==scissors && computerSelection==rock){
+//   console.log('You lose, rock beats scissors')
+// }
+
+// else if(playerSelection==scissors && computerSelection==paper){
+//   console.log('You win,scissors beats paper')
+// }
+//  else if(playerSelection==paper && computerSelection==scissors)
+//  {console.log('You lose, scissors beats paper')}
+
+//  else{roundOne(computerSelection,playerSelection)}
+
+
+
+
+//poss== rock &paper &rock & scissors & paper & scissors
 
 //sep//
-let rock ='rock';
-let scissors ='scissors';
-let paper='paper';
-let playerSelection ='you are the winner';
-let computerSelection ='you are the winner';
-//sep//
 
-function getComputerChoice(rock,paper,scissors) {
-    let result=Math.floor(Math.random()*100) ;//decides random number
-    if (result>=0 && result<=49) {
-          return rock;
-           
-         } 
-         
-        else if(result>=50 && result<=74){ 
-             return scissors;
-        
-        }
-      else if(result>=75 && result<=90 ){
-       return paper;
-       }
-   }
+
+
+
+
+
+
+
+
+
+
+
+let rock = "rock"
+let paper = "paper";
+let scissors = "scissors";
+let computerSelection;
+let playerSelection;
+
+function numGen() {
+  let randoNum = Math.floor(Math.random() * 100);
+  return randoNum;
+}
+
+let getComputerChoice = function (rock, paper, scissors, numGen) {
+  let result = numGen();
+  if (result > 20 && result < 45) {
+    return paper;
+  } else if (result > 75 && result <= 80) {
+    return scissors;
+  } else {
+    return rock;
+  }
+}
 
 function roundOne(playerSelection, computerSelection) {
-   if ( playerSelection===true && rock===true && paper===true) {
-      'you win rock beats paper'
-      
-   }
-else if(computerSelection===true && rock===true && paper===true){ 'you win rock beats paper'}
+  playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+console.log(playerSelection)
 
-else if(computerSelection===true&& rock===true && scissors===true){'you lose rock beats scissors'}
-
-else if(computerSelection===true && paper===true && scissors===true){'you lose scissors beats paper'}
+  computerSelection = getComputerChoice(rock, paper, scissors, numGen);
+  console.log(computerSelection);
 
 
-   else if (playerSelection===true && rock===true && scissors===true)
-   { 'you lose scissors beats rock'}
 
-    else if ( playerSelection===true && paper===true && scissors===true){
-'you lose scissors beats paper'
-    }
-   else{'you tied try again'}
-   }
+  if (playerSelection === rock && computerSelection === paper) {
+    console.log('You lose, paper beats rock');
+  } else if (playerSelection === paper && computerSelection === rock) {
+    console.log('You win, paper beats rock');
+  } else if (playerSelection === rock && computerSelection === scissors) {
+    console.log('You win, rock beats scissors');
+  } else if (playerSelection === scissors && computerSelection === rock) {
+    console.log('You lose, rock beats scissors');
+  } else if (playerSelection === scissors && computerSelection === paper) {
+    console.log('You win, scissors beats paper');
+  } else if (playerSelection === paper && computerSelection === scissors) {
+    console.log('You lose, scissors beats paper');
+  } else {
+    roundOne(computerSelection, playerSelection);
+  }
+}
 
-roundOne(getComputerChoice)
+const rck = document.getElementById('rock')  //rock button
+const ppr= document.getElementById('paper') //paper button
+const scsr= document.getElementById('scissors') //scissors button 
+
